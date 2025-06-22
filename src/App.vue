a4e931d1f0344f60cf5e992340a953b3bc44b0be
Jay <85284246+Yu-jae-min@users.noreply.github.com>
2025-06-22 21:10:44 +0900

    feat: 선언적 렌더링

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="HelloWorld Text" />
+    <TestOne />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import TestOne from "./components/TestOne.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    TestOne,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/TestOne.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>{{ message }}</div>
+</template>
+
+<script>
+export default {
+  name: "TestOne",
+  data() {
+    return {
+      message: "엥?",
+    };
+  },
+};
+</script>
