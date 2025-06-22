dc99442f6cbca49fd6e714e0b063266cd82b14b5
Jay <85284246+Yu-jae-min@users.noreply.github.com>
2025-06-22 22:18:23 +0900

    feat : route 수정

--- src/constants/route.js
+++ src/constants/route.js
@@ -1,4 +1,7 @@
+// home
 export const HOME_PATH = "/";
+
+// test
 export const TEST_ONE_PATH = "/1";
 export const TEST_TWO_PATH = "/2";
 export const TEST_THREE_PATH = "/3";
--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,13 @@ import TestOne from "@/components/TestOne.vue";
 import TestTwo from "@/components/TestTwo.vue";
 import TestThree from "@/components/TestThree.vue";
 import TestFour from "@/components/TestFour.vue";
+import {
+  HOME_PATH,
+  TEST_FOUR_PATH,
+  TEST_ONE_PATH,
+  TEST_THREE_PATH,
+  TEST_TWO_PATH,
+} from "@/constants/route";
 
 Vue.use(Router);
 
@@ -12,28 +19,28 @@ export default new Router({
   mode: "history",
   routes: [
     {
-      path: "/",
+      path: HOME_PATH,
       name: "MainHome",
       component: MainHome,
       props: () => ({ msg: "Hello from route!" }),
     },
     {
-      path: "/1",
+      path: TEST_ONE_PATH,
       name: "TestOne",
       component: TestOne,
     },
     {
-      path: "/2",
-      name: "TestOne",
+      path: TEST_TWO_PATH,
+      name: "TestTwo",
       component: TestTwo,
     },
     {
-      path: "/3",
+      path: TEST_THREE_PATH,
       name: "TestThree",
       component: TestThree,
     },
     {
-      path: "/4",
+      path: TEST_FOUR_PATH,
       name: "TestFour",
       component: TestFour,
     },
